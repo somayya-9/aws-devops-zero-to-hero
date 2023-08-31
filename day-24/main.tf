297cc97bb593ca44ac5c412149cd949a6262b1f8
somayya-9 <131620813+somayya-9@users.noreply.github.com>
2023-08-31 12:52:22 +0100

    Update main.tf

--- day-24/main.tf
+++ day-24/main.tf
@@ -5,14 +5,14 @@ resource "aws_vpc" "myvpc" {
 resource "aws_subnet" "sub1" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-west-2a"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "sub2" {
   vpc_id                  = aws_vpc.myvpc.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1b"
+  availability_zone       = "eu-west-2b"
   map_public_ip_on_launch = true
 }
 
